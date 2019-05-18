a28b5759e6ecee7b2e416d623a9a02853ed2501d
Bradley Jensen <bradley13@gmail.com>
2019-05-18 11:12:19 -0400

    fixed article schema

--- client/src/Components/NewsFeed/index.js
+++ client/src/Components/NewsFeed/index.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 
 class NewsFeed extends Component {
-  render() {
-    return (
-      <div>News Feed</div>
-    );
-  }
+	render() {
+		return <div>News Feed</div>;
+	}
 }
 
 export default NewsFeed;
--- client/src/Components/Search/index.js
+++ client/src/Components/Search/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './style.css';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import { Container, Row, Col } from 'react-grid-system';
 
 class Search extends Component {
 	state = {
-    search: '',
-    category: 'New York Times'
+		search: '',
+		category: 'New York Times',
 	};
 
 	onInputChange = e => {
@@ -31,51 +31,59 @@ class Search extends Component {
 		return this.state.search !== '';
 	};
 
-
-
-render() {
-	return (
-<Container>
-  <form>
-	<Row style={{border: "solid grey 2px", marginTop: "30px", borderRadius:"10px 10px 0 0"}}>
-	
-	<Col xs={12} md={5}>
-			<label className="my-auto mx-1">
-		Search:
-			<input type="text" name="search" value={this.state.search} onChange={this.onInputChange} />
-		</label>
-	</Col>
-		<Col xs={12} md={3}>
-			<label className="my-auto mx-1">
-		Source:
-			<select name="category" onChange={this.onInputChange} value={this.state.category} className="form-control select-fix">
-			<option value="New York Times">New York Times</option>
-			<option value="NPR">NPR</option>
-			</select>
-		</label>
-	</Col>
-		<Col xs={12} md={2}>
-			<button className="btn btn-small waves-effect waves-light hoverable red accent-3" 
-					onClick={this.onFormSubmit} 
-					type="submit"
-					style={{
-						width: "150px",
-						borderRadius: "3px",
-						letterSpacing: "1.5px",
-						marginTop: "1rem"
-					  }}>
-				Search
-		</button>
-	</Col>
-	<Col xs={12} md={2}>
-			<Link to="/" className="btn-flat waves-effect">
-        		<i className="material-icons right">arrow_forward_ios</i>Archive
-      		</Link>
-	</Col>
-
-	</Row>
-  </form>
-</Container>
+	render() {
+		return (
+			<Container>
+				<form>
+					<Row style={{ border: 'solid grey 2px', marginTop: '30px', borderRadius: '10px 10px 0 0' }}>
+						<Col xs={12} md={5}>
+							<label className="my-auto mx-1">
+								Search:
+								<input
+									type="text"
+									name="search"
+									value={this.state.search}
+									onChange={this.onInputChange}
+								/>
+							</label>
+						</Col>
+						<Col xs={12} md={3}>
+							<label className="my-auto mx-1">
+								Source:
+								<select
+									name="category"
+									onChange={this.onInputChange}
+									value={this.state.category}
+									className="form-control select-fix"
+								>
+									<option value="New York Times">New York Times</option>
+									<option value="NPR">NPR</option>
+								</select>
+							</label>
+						</Col>
+						<Col xs={12} md={2}>
+							<button
+								className="btn btn-small waves-effect waves-light hoverable red accent-3"
+								onClick={this.onFormSubmit}
+								type="submit"
+								style={{
+									width: '150px',
+									borderRadius: '3px',
+									letterSpacing: '1.5px',
+									marginTop: '1rem',
+								}}
+							>
+								Search
+							</button>
+						</Col>
+						<Col xs={12} md={2}>
+							<Link to="/" className="btn-flat waves-effect">
+								<i className="material-icons right">arrow_forward_ios</i>Archive
+							</Link>
+						</Col>
+					</Row>
+				</form>
+			</Container>
 		);
 	}
 }
