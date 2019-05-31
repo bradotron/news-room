2bdac8a26e76dda91ee0595a545100ff317c66fa
stefangreene <stefangreene@comcast.net>
2019-05-31 10:56:12 -0400

    front end adjustments

--- client/src/Components/Profile/index.js
+++ client/src/Components/Profile/index.js
@@ -5,24 +5,25 @@ import './style.css';
 
 function Profile(props) {
 	return (
-		<Container className="profile">
+		<Container className="profile" >
 			<Row className="profile-row">
-				<Col xs={12} md={7} className="back-button">
+				<Col xs={12} md={7} className="back">
+				<div className="back-button">
 					<Link to="/" className="btn-flat waves-effect light-blue-text">
 						<i className="material-icons left">arrow_back_ios</i> Back to home
 					</Link>
-					<h4 className="headline">
-						<b style={{ color: 'red', fontSize: '60px' }}>Search</b>
-						<span style={{ fontSize: '55px' }}> the News</span>
+				</div>
+				<div className="headline">
+					<h4>
+						<b style={{ color: 'red', fontSize: '55px' }}>Search</b>
+						<span style={{ fontSize: '50px' }}> the News</span>
 					</h4>
+				</div>
 				</Col>
 
-				<Col xs={12} md={3} />
-				<Col xs={12} md={2} style={{ justifyItems: 'center' }}>
-					<img src="https://via.placeholder.com/100" alt="" style={{ borderRadius: '80px' }} />
-					<hr />
-					<p>{props.user.name}</p>
-					<p>{props.user.email}</p>
+				<Col xs={12} md={3} className="user-info">
+				<p className="user-name">{props.user.name}</p>
+					<p className="user-email">{props.user.email}</p>
 					<button id="logout-btn"
 						onClick={props.onLogout}
 						type="logout"
@@ -30,6 +31,14 @@ function Profile(props) {
 					>
 						Log out
 					</button>
+
+				</Col>
+				<Col xs={12} md={2} style={{ justifyItems: 'center' }}>
+				<hr />
+			
+					<img src="https://via.placeholder.com/100" alt="" style={{ borderRadius: '80px' }} />
+					
+
 				</Col>
 			</Row>
 		</Container>
